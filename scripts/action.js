773cd7da5090d8c8ef2e4acfb15139838e7ba97b
Nikos Kleidis <n.kleidis@ferryhopper.com>
2024-04-03 16:20:25 +0300

    differentiate actions based on target branches

--- scripts/action.js
+++ scripts/action.js
@@ -9,6 +9,12 @@ async function run() {
   console.log("Retrieving PR title...")
   console.log(`PR title:: ${getTitle()}`);
   console.log(JSON.stringify(github.context.payload.pull_request))
+  const targetBranch = github.context.payload.pull_request.base.ref;
+  if(targetBranch === 'master') {
+    console.log('should move tickets to in review column');
+  } else {
+    console.log('should move tickets to in progress column');
+  }
 }
 
 run().catch(e => core.setFailed(e));
