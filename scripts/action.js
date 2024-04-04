e314ad6693911793707612f63eea9c87a8fe08e5
Nikos Kleidis <n.kleidis@ferryhopper.com>
2024-04-04 16:58:45 +0300

    Apparently it does work

--- scripts/action.js
+++ scripts/action.js
@@ -1,5 +1,5 @@
-import github from '@actions/github';
-import core from '@actions/core';
+const github = require('@actions/github');
+const core = require('@actions/core');
 
 const mainBranch = 'main';
 
@@ -14,22 +14,28 @@ async function run() {
   const targetBranch = github.context.payload.pull_request.base.ref;
   const isMerged = github.context.payload.pull_request.merged;
   const isClosed = github.context.payload.pull_request.state === 'closed';
-  if (isClosed && !isMerged) {
-    console.log('PR is closed but not merged. No action needed.');
-    return;
-  }
 
-  let newTicketStatus = 'In progress';
-  if(targetBranch === mainBranch) {
+  let newTicketStatus = 'IN PROGRESS';
+  if (isClosed && !isMerged) {
+    newTicketStatus = 'OPEN'
+  } else if(targetBranch === mainBranch) {
     if(isMerged) {
-      console.log('should move tickets to QA column');
       newTicketStatus = 'QA';
     } else {
-      console.log('should move tickets to in review column');
-      newTicketStatus = 'In Review';
+      newTicketStatus = 'IN REVIEW';
     }
   }
   console.log('New tickets status:', newTicketStatus);
 }
 
 run().catch(e => core.setFailed(e));
+
+/*
+    Action  | Target  | New Status
+    -------------------------------
+    Open    | any     | IN REVIEW
+    merged  | master  | QA
+    merged  | other   | Do nothing
+    closed  | any     | OPEN
+    draft   | any     | IN PROGRESS
+ */
