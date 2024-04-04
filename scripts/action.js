73b53107cb02676f2029fcb15c30a5a6a46c508d
Nikos Kleidis <n.kleidis@ferryhopper.com>
2024-04-04 16:55:37 +0300

    Use import instead of require. Does it work??

--- scripts/action.js
+++ scripts/action.js
@@ -1,5 +1,5 @@
-const github = require('@actions/github');
-const core = require('@actions/core');
+import github from '@actions/github';
+import core from '@actions/core';
 
 const mainBranch = 'main';
 
